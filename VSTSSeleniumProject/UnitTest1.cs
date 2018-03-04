e40b4200c1cf5f65af124fa19073b26fcea7d140
demdeni <demdeni@gmail.com>
2018-03-04 09:53:34 +0200

    RunChromeDriverHeadless

--- VSTSSeleniumProject/UnitTest1.cs
+++ VSTSSeleniumProject/UnitTest1.cs
@@ -17,36 +17,56 @@
 
         private string baseURL = "https://syneron-candela.com/int";
         public IWebDriver driver;
-        public IWebDriver driver1;
+        public RemoteWebDriver driver1;
         private string browser;
         public TestContext TestContext { get; set; }
 
         [TestMethod]
-        [TestCategory("Selenium")]
-        [TestCategory("Headless")]
-        public void BingSearch_Headless()
+        public void RunChromeDriverHeadless()
         {
-            driver1 = new PhantomJSDriver();
-            driver1.Navigate().GoToUrl(baseURL);
+            ChromeOptions option = new ChromeOptions();
+            option.AddArgument("--headless");
+            driver = new ChromeDriver(option);
 
-            var element = driver1.FindElement(By.ClassName("wlcm-title"));
+            driver.Navigate().GoToUrl(baseURL);
+
+            var element = driver.FindElement(By.ClassName("wlcm-title"));
             Assert.IsTrue(element.Displayed);
 
-            driver1.FindElement(By.Id("cboxClose")).Click();
+            driver.FindElement(By.Id("cboxClose")).Click();
 
-            var logo = driver1.FindElement(By.Id("logo"));
+            var logo = driver.FindElement(By.Id("logo"));
             Assert.IsTrue(logo.Displayed);
-
-            //driver1.FindElement(By.Id("txtEmail")).SendKeys("andy@andykelk.net");
-            //driver1.FindElement(By.Id("txtPassword")).SendKeys("xxxxx");
-            //driver1.FindElement(By.Id("imgbtnLogin")).Submit();
-
-            //var driver = new PhantomJSDriver();
-            //driver.Navigate(baseURL);
-            //page.GoTo().SearchFor("Brian Harry Blog");
-            //Assert.AreEqual("Brian Harry's blog - Site Home - MSDN Blogs", page.FirstSearchResult.Text, "site done broke.");
-            //driver.Quit();
         }
+
+
+
+        //[TestMethod]
+        //[TestCategory("Selenium")]
+        //[TestCategory("Headless")]
+        //public void BingSearch_Headless()
+        //{
+        //    driver1 = new PhantomJSDriver();
+        //    driver1.Navigate().GoToUrl(baseURL);
+
+        //    var element = driver1.FindElement(By.ClassName("wlcm-title"));
+        //    Assert.IsTrue(element.Displayed);
+
+        //    driver1.FindElement(By.Id("cboxClose")).Click();
+
+        //    var logo = driver1.FindElement(By.Id("logo"));
+        //    Assert.IsTrue(logo.Displayed);
+
+        //    driver1.FindElement(By.Id("txtEmail")).SendKeys("andy@andykelk.net");
+        //    driver1.FindElement(By.Id("txtPassword")).SendKeys("xxxxx");
+        //    driver1.FindElement(By.Id("imgbtnLogin")).Submit();
+
+        //    var driver = new PhantomJSDriver();
+        //    driver.Navigate(baseURL);
+        //    page.GoTo().SearchFor("Brian Harry Blog");
+        //    Assert.AreEqual("Brian Harry's blog - Site Home - MSDN Blogs", page.FirstSearchResult.Text, "site done broke.");
+        //    driver.Quit();
+        //}
 
 
 
