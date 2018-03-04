1dd937aadb6a74e28e64a23726adcfadb2068e64
demdeni <demdeni@gmail.com>
2018-03-04 15:08:28 +0200

    ;uikyigfuldkyf

--- VSTSSeleniumProject/UnitTest1.cs
+++ VSTSSeleniumProject/UnitTest1.cs
@@ -40,23 +40,23 @@
             driver.Quit();
         }
 
-        //[TestMethod]
-        //[TestCategory("Selenium")]
-        //[TestCategory("Headless")]
-        //public void BingSearch_Headless()
-        //{
-        //    var driver = new PhantomJSDriver();
-        //    driver.Navigate().GoToUrl(baseURL);
+        [TestMethod]
+        [TestCategory("Selenium")]
+        [TestCategory("Headless")]
+        public void BingSearch_Headless()
+        {
+            var driver = new PhantomJSDriver();
+            driver.Navigate().GoToUrl(baseURL);
 
-        //    var element = driver.FindElement(By.ClassName("wlcm-title"));
-        //    Assert.IsTrue(element.Displayed);
+            var element = driver.FindElement(By.ClassName("wlcm-title"));
+            Assert.IsTrue(element.Displayed);
 
-        //    driver.FindElement(By.Id("cboxClose")).Click();
+            driver.FindElement(By.Id("cboxClose")).Click();
 
-        //    var logo = driver.FindElement(By.Id("logo"));
-        //    Assert.IsTrue(logo.Displayed);
-        //    driver.Quit();
-        //}
+            var logo = driver.FindElement(By.Id("logo"));
+            Assert.IsTrue(logo.Displayed);
+            driver.Quit();
+        }
 
         //[TestMethod]
         //[TestCategory("Selenium")]
@@ -66,54 +66,54 @@
         //    driver1 = new ChromeDriver();
         //    driver1.Navigate().GoToUrl(baseURL);
 
-            //    var element = driver1.FindElement(By.ClassName("wlcm-title"));
-            //    Assert.IsTrue(element.Displayed);
+        //    var element = driver1.FindElement(By.ClassName("wlcm-title"));
+        //    Assert.IsTrue(element.Displayed);
 
-            //    driver1.FindElement(By.Id("cboxClose")).Click();
+        //    driver1.FindElement(By.Id("cboxClose")).Click();
 
-            //    var logo = driver1.FindElement(By.Id("logo"));
-            //    Assert.IsTrue(logo.Displayed);
+        //    var logo = driver1.FindElement(By.Id("logo"));
+        //    Assert.IsTrue(logo.Displayed);
 
-            //    //driver1.FindElement(By.Id("txtEmail")).SendKeys("andy@andykelk.net");
-            //    //driver1.FindElement(By.Id("txtPassword")).SendKeys("xxxxx");
-            //    //driver1.FindElement(By.Id("imgbtnLogin")).Submit();
+        //    //driver1.FindElement(By.Id("txtEmail")).SendKeys("andy@andykelk.net");
+        //    //driver1.FindElement(By.Id("txtPassword")).SendKeys("xxxxx");
+        //    //driver1.FindElement(By.Id("imgbtnLogin")).Submit();
 
-            //    //var driver = new PhantomJSDriver();
-            //    //driver.Navigate(baseURL);
-            //    //page.GoTo().SearchFor("Brian Harry Blog");
-            //    //Assert.AreEqual("Brian Harry's blog - Site Home - MSDN Blogs", page.FirstSearchResult.Text, "site done broke.");
-            //    //driver.Quit();
-            //}
+        //    //var driver = new PhantomJSDriver();
+        //    //driver.Navigate(baseURL);
+        //    //page.GoTo().SearchFor("Brian Harry Blog");
+        //    //Assert.AreEqual("Brian Harry's blog - Site Home - MSDN Blogs", page.FirstSearchResult.Text, "site done broke.");
+        //    //driver.Quit();
+        //}
 
 
 
-            //[TestMethod]
-            //[TestCategory("Selenium")]
-            //[Priority(1)]
-            //[Owner("Chrome")]
-            //public void FirstTest()
-            //{
-            //    driver = new ChromeDriver();
-            //    Assert.IsNotNull(driver);
-            //    driver.Manage().Window.Maximize();
-            //    driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(30));
-            //    driver.Navigate().GoToUrl(baseURL);
+        //[TestMethod]
+        //[TestCategory("Selenium")]
+        //[Priority(1)]
+        //[Owner("Chrome")]
+        //public void FirstTest()
+        //{
+        //    driver = new ChromeDriver();
+        //    Assert.IsNotNull(driver);
+        //    driver.Manage().Window.Maximize();
+        //    driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(30));
+        //    driver.Navigate().GoToUrl(baseURL);
 
-            //    var element = driver.FindElement(By.ClassName("wlcm-title"));
-            //    Assert.IsTrue(element.Displayed);
+        //    var element = driver.FindElement(By.ClassName("wlcm-title"));
+        //    Assert.IsTrue(element.Displayed);
 
-            //    driver.FindElement(By.Id("cboxClose")).Click();
+        //    driver.FindElement(By.Id("cboxClose")).Click();
 
-            //    var logo = driver.FindElement(By.Id("logo"));
-            //    Assert.IsTrue(logo.Displayed);
+        //    var logo = driver.FindElement(By.Id("logo"));
+        //    Assert.IsTrue(logo.Displayed);
 
-            //}
+        //}
 
-            //[TestCleanup()]
-            //public void MyTestCleanup()
-            //{
-            //    driver.Quit();
-            //}
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //    driver.Quit();
+        //}
 
         [TestInitialize()]
         public void MyTestInitialize()
